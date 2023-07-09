a749d2bacc1b307c8c1d77f13b27bdf6a6ac23eb
Abhishek Chaudhary <abhishekchaudhary@N0467-AbhishekC.local>
2023-07-09 22:15:54 +0530

    commit4

--- src/main/java/com/increff/invoice/service/InvoiceService.java
+++ src/main/java/com/increff/invoice/service/InvoiceService.java
@@ -15,42 +15,34 @@ import java.nio.file.Paths;
 import java.util.Base64;
 import java.util.List;
 
-
-
 @Service
 public class InvoiceService {
     public ResponseEntity<byte[]> getInvoice(InvoiceData form) throws IOException {
         List<OrderItem> items = form.getOrderItemDataList();
-        Double amt = 0.0;
-        for (OrderItem i : items) {
-            Double cur = 0.0;
-            cur = i.getSellingPrice() * i.getQuantity();
-            amt += cur;
-            i.setAmt(cur);
+        double totalAmount = 0.0;
+        for (OrderItem item : items) {
+            double itemAmount = item.getSellingPrice() * item.getQuantity();
+            totalAmount += itemAmount;
+            item.setAmt(itemAmount);
         }
-        form.setAmount(amt);
-        CreateXMLFileJava createXMLFileJava = new CreateXMLFileJava();
+        form.setAmount(totalAmount);
 
+        CreateXMLFileJava createXMLFileJava = new CreateXMLFileJava();
         createXMLFileJava.createXML(form);
 
         PDFFromFOP pdfFromFOP = new PDFFromFOP();
-
         pdfFromFOP.createPDF();
 
-
-        String _filename = "./generated/invoice_" + form.getOrderId() + ".pdf";
+        String filename = "invoice_" + form.getOrderId() + ".pdf";
         Path pdfPath = Paths.get("./generated/invoice.pdf");
-
-        byte[] contents = Base64.getEncoder().encode(Files.readAllBytes(pdfPath));
+        byte[] contents = Files.readAllBytes(pdfPath);
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_PDF);
-        // Here you have to set the actual filename of your pdf
-        String filename = "output.pdf";
         headers.setContentDispositionFormData(filename, filename);
         headers.setCacheControl("must-revalidate, post-check=0, pre-check=0");
-        ResponseEntity<byte[]> response = new ResponseEntity<>(contents, headers, HttpStatus.OK);
-        return response;
 
+        ResponseEntity<byte[]> response = new ResponseEntity<>(Base64.getEncoder().encode(contents), headers, HttpStatus.OK);
+        return response;
     }
 }
--- src/main/java/com/increff/invoice/service/PDFFromFOP.java
+++ src/main/java/com/increff/invoice/service/PDFFromFOP.java
@@ -5,51 +5,47 @@ import org.apache.fop.apps.*;
 import javax.xml.transform.*;
 import javax.xml.transform.sax.SAXResult;
 import javax.xml.transform.stream.StreamSource;
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
 
 public class PDFFromFOP {
-    private String xmlFilePath="./src/main/resources/xml/Invoice.xml" ;
-    private String xslFilePath="./src/main/resources/xsl/Invoice.xsl" ;
-    private String generatedDirPath="./generated" ;
-    public void createPDF() {
+    private final String xmlFilePath = "./src/main/resources/xml/Invoice.xml";
+    private final String xslFilePath = "./src/main/resources/xsl/Invoice.xsl";
+    private final String generatedDirPath = "./generated";
 
+    public void createPDF() {
         try {
-            File xmlfile = new File(xmlFilePath);
-            File xsltfile = new File(xslFilePath);
+            File xmlFile = new File(xmlFilePath);
+            File xsltFile = new File(xslFilePath);
             File pdfDir = new File(generatedDirPath);
             pdfDir.mkdirs();
             File pdfFile = new File(pdfDir, "invoice.pdf");
-            // configure fopFactory as desired
-            final FopFactory fopFactory = FopFactory.newInstance(new File(".").toURI());
+
+            FopFactory fopFactory = FopFactory.newInstance(new File(".").toURI());
             FOUserAgent foUserAgent = fopFactory.newFOUserAgent();
-            // configure foUserAgent as desired
-            // Setup output
+
             OutputStream out = new FileOutputStream(pdfFile);
-            out = new java.io.BufferedOutputStream(out);
+            out = new BufferedOutputStream(out);
+
             try {
-                // Construct fop with desired output format
-                Fop fop;
-                fop = fopFactory.newFop(MimeConstants.MIME_PDF, foUserAgent, out);
-                // Setup XSLT
-                TransformerFactory factory = TransformerFactory.newInstance();
-                Transformer transformer = factory.newTransformer(new StreamSource(xsltfile));
-                // Setup input for XSLT transformation
-                Source src = new StreamSource(xmlfile);
-                // Resulting SAX events (the generated FO) must be piped through to FOP
+                Fop fop = fopFactory.newFop(MimeConstants.MIME_PDF, foUserAgent, out);
+
+                TransformerFactory transformerFactory = TransformerFactory.newInstance();
+                Transformer transformer = transformerFactory.newTransformer(new StreamSource(xsltFile));
+
+                Source src = new StreamSource(xmlFile);
                 Result res = new SAXResult(fop.getDefaultHandler());
-                // Start XSLT transformation and FOP processing
+
                 transformer.transform(src, res);
-            } catch (FOPException | TransformerException e) {
-                // TODO Auto-generated catch block
+            } catch (Exception e) {
                 e.printStackTrace();
             } finally {
                 out.close();
             }
-        }catch(Exception exp){
-            exp.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
-
 }
